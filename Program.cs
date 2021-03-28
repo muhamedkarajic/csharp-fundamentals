f628876ae0c8326a2df06c1658a6761717b50868
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-03-28 13:56:38 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,14 +11,15 @@ namespace CSharpFundamentals
             var numbers = new List<double>() { 12.2, 13.3, 10.4, 1.6 };
             numbers.Add(22.1);
 
-if(numbers.Count > 0)
-{
-    Console.WriteLine(numbers.Sum() / numbers.Count);
-}
-else {
-    Console.WriteLine("List is empty");
-}
-            
+            if (numbers.Count > 0)
+            {
+                Console.WriteLine(numbers.Sum() / numbers.Count);
+            }
+            else
+            {
+                Console.WriteLine("List is empty");
+            }
+
         }
     }
 }
