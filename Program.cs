646bee84deca5a88d37a13697f5c29c5bbfdcc62
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-03-28 14:37:26 +0200

    Computing Statistics

--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@ namespace CSharpFundamentals
         static void Main(string[] args)
         {
             var book = new Book("New book");
-            book.AddGrade(189.2);
+            book.AddGrade(89.89);
+            book.AddGrade(25.42);
+            book.AddGrade(92.11);
+            book.ShowStatistics();
         }
     }
 }
--- models/Book.cs
+++ models/Book.cs
@@ -20,8 +20,26 @@ namespace Models
                 throw new Exception("Grade can't be higher then 100.0");
             else if(grade < 0)
                 throw new Exception("Grade can't be less then 0.0");
-                
+
             grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var lowestGrade = double.MaxValue;
+            var highestGrade = double.MinValue;
+            
+            var result = 0.0;
+            foreach (var grade in grades)
+            {
+                lowestGrade = Math.Min(grade, lowestGrade);
+                highestGrade = Math.Max(grade, highestGrade);
+                result += grade;
+            }
+
+            Console.WriteLine($"The lowest grade is {lowestGrade}");
+            Console.WriteLine($"The highest grade is {highestGrade}");
+            Console.WriteLine($"The average grade is {result / grades.Count:N2}");
         }
     }
 }
