413b6e3cadbd368921490ce4255ddf30e2467bcb
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-03-28 14:11:02 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,21 +4,16 @@ using System.Linq;
 
 namespace CSharpFundamentals
 {
+    class Book 
+    {
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            var numbers = new List<double>() { 12.2, 13.3, 10.4, 1.6 };
-            numbers.Add(22.1);
-
-            if (numbers.Count > 0)
-            {
-                Console.WriteLine($"The average of the numbers is: {numbers.Sum() / numbers.Count:N1}");
-            }
-            else
-            {
-                Console.WriteLine("List is empty");
-            }
+            var book = new Book();
         }
     }
 }
