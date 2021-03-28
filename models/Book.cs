3b7dab9d9d3fbb8e866232ba6e15560a9ef3e805
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-03-28 14:23:11 +0200

    Requiring Constructor Parameters

--- models/Book.cs
+++ models/Book.cs
@@ -5,12 +5,15 @@ namespace Models
 {
     public class Book 
     {
-        public Book()
+        public Book(string name)
         {
+            this.name = name;
             grades = new List<double>();
         }
 
+        string name;
         List<double> grades;
+
         public void AddGrade(double grade)
         {
             grades.Add(grade);
