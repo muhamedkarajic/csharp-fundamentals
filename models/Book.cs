3dced3c29accd2a9f3fe5b8e5f81f69a570620d6
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-03-28 14:18:45 +0200

    Defining a Field

--- models/Book.cs
+++ models/Book.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
+
 namespace Models
 {
     public class Book 
     {
+        List<double> grades;
         public void AddGrade(double grade)
         {
-            
+            grades.Add(grade);
         }
     }
 }
