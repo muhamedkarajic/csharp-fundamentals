2a58ff7281f946af6ae42c16ea306f2fb3b84822
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-03-28 14:15:05 +0200

    using new namespaces and reorganizing files

--- Program.cs
+++ Program.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Models;
 
 namespace CSharpFundamentals
 {
-    class Book 
-    {
-
-    }
-
     class Program
     {
         static void Main(string[] args)
