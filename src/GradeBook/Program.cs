e7cc307c384bd597a2a43753e022ef8f972d62f4
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-04-03 18:59:43 +0200

    Different approach for adding input values

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -5,15 +5,21 @@ namespace CSharpFundamentals
 {
     class Program
     {
-        static void Main(string[] grades)
+        static void Main(string[] args)
         {
             var book = new Book("New book");
             
-            foreach (var grade in grades)
+            while (true)
             {
-                book.AddGrade(Double.Parse(grade));
+                Console.Write("Please enter a grade ('q' to quit): ");
+                var input = Console.ReadLine();
+                if(input == "q")
+                    break;
+                
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
             }
-            
+
             book.GetStatistics();
             
             var statistics = book.GetStatistics();
