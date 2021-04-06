331394a6842e9c2d125b33cc15d0d79c088ba052
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2021-04-06 23:26:39 +0200

    Chaining Constructors

--- src/GradeBook/models/Book.cs
+++ src/GradeBook/models/Book.cs
@@ -7,6 +7,11 @@ namespace Models
 
     public class NamedObject
     {
+        public NamedObject(string name)
+        {
+            Name = name;
+        }
+
         public string Name { get; set; }
 
     }
@@ -14,9 +19,8 @@ namespace Models
     public class Book : NamedObject
     {
 
-        public Book(string name)
+        public Book(string name): base(name)
         {
-            Name = name;
             grades = new List<double>();
         }
 
